0fa8cbb81019ce7de507f4cafbbe6164e37d57ad
ekenf <38294319+ekenf@users.noreply.github.com>
2022-11-30 16:55:50 +0300

    Add files via upload

---
+++ AddressView.swift
@@ -0,0 +1,42 @@
+//
+//  AddressView.swift
+//  Cupcake
+//
+//  Created by Furkan Eken on 29.11.2022.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    @ObservedObject var order : Order
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+            
+            Section {
+                NavigationLink {
+                    CheckoutView(order: order)
+                } label: {
+                    Text("Check out")
+                }
+                .disabled(order.hasValidAddress == false)
+            }
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AddressView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            AddressView(order: Order())
+        }
+    }
+}
---
+++ CupcakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeApp.swift
+//  Cupcake
+//
+//  Created by Furkan Eken on 22.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
